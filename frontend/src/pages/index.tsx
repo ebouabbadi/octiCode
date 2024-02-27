89b924da3bd5d5c800700ebe85eb91b007b70ba5
ebouabba <ebouabbadi@gmail.com>
2024-02-27 18:04:11 +0100

    push

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -105,7 +105,6 @@ const Home = () => {
           animate={{ y: 0, opacity: 1 }}
           exit={{ y: -20, opacity: 0 }}
           transition={{ duration: 1 }}
-
           >
             {renderUsers()}
           </motion.div>
